06488fef43d009079e5d50a23a3d0ce206f57082
ChieF-TroN <allengentry@gmail.com>
2019-09-30 06:20:51 -0400

    Added Bank Check, Fixed Errors
    Fixed issue with the server comparing the bank/wallets with other user accounts.

--- server.lua
+++ server.lua
@@ -2,9 +2,11 @@
 
 -----------------------------------------------------------------
 -- Config
-local moddedMoney = 25000000 -- Amount of money greater than to trigger Kick/Ban
-local kickorban = "ban" -- Use "ban" or "kick"
-local useDiscord = false -- Use Discord Webhook? Input your discord webook link where it says INPUT LINK HERE on line 35 and 42.
+local moddedMoney = 25000000 	-- Amount of money greater than to trigger Kick/Ban (Wallet)
+local moddedbMoney = 100000000 	-- Amount of money greater than to trigger Kick/Ban (Bank)
+local BankCheck = false			-- Do the check on the bank as well?
+local kickorban = "ban" 		-- Use "ban" or "kick"
+local useDiscord = false 		-- Use Discord Webhook? Input your discord webook link where it says INPUT LINK HERE on line 35 and 42.
 -----------------------------------------------------------------
 
 local Proxy = module("vrp", "lib/Proxy")
@@ -13,45 +15,88 @@ local Tunnel =  module("vrp", "lib/Tunnel")
 vRP = Proxy.getInterface("vRP")
 Tunnel.bindInterface("vrp_banksecurity",vRPbs)
 
-local usercurrentmoney = nil
-local useroldmoney = nil
+local useroldmoney = {}
+local useroldbmoney = {}
+local walletmoney = {}
+local bankmoney = {}
 local canGiveMoney = false
 
 vRPbs = {}
-function vRPbs.GetWallet(source)
-	local user_id = vRP.getUserId({source})
-	usercurrentmoney = vRP.getMoney({user_id})
-	if useroldmoney == nil then
-		useroldmoney = usercurrentmoney
-	elseif useroldmoney ~= nil then
-		if usercurrentmoney ~= nil and canGiveMoney == false then
-			local difference = usercurrentmoney - useroldmoney
-			if usercurrentmoney > (useroldmoney + moddedMoney) then
-				print("UserID: " ..user_id.. " Has received more than "..moddedMoney.." in <5 seconds. Old Wallet: " ..useroldmoney.. " New Wallet: " ..usercurrentmoney.. " Value: $" ..difference)
-				if useDiscord == true then
-					if kickorban == "ban" then
-						vRP.getUserIdentity({user_id, function(identity)
-							local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedMoney.." in Cash in <5 seconds. Old Wallet: $" ..useroldmoney.. " New Wallet: $" ..usercurrentmoney.. " Value: $" ..difference.. " and was automatically banned. @everyone"
-							PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+
+function vRPbs.GetMoneyStatus(userid)
+	local user_id = userid
+	walletmoney[user_id] = vRP.getMoney({user_id})
+	bankmoney[user_id] = vRP.getBankMoney({user_id})
+	if useroldmoney[user_id] == nil then
+		useroldmoney[user_id] = walletmoney[user_id]
+	elseif useroldmoney[user_id] ~= nil then
+		if walletmoney[user_id] ~= nil and canGiveMoney == false then
+			local difference = walletmoney[user_id] - useroldmoney[user_id]
+			if useroldmoney[user_id] ~= bankmoney[user_id] then
+				if walletmoney[user_id] > (useroldmoney[user_id] + moddedMoney) then
+					print("UserID: " ..user_id.. " Has received more than "..moddedMoney.." in <5 seconds. Old Wallet: " ..useroldmoney[user_id].. " New Wallet: " ..walletmoney[user_id].. " Value: $" ..difference)
+					if useDiscord == true then
+						if kickorban == "ban" then
+							vRP.getUserIdentity({user_id, function(identity)
+								local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedMoney.." in Cash in <5 seconds. Old Wallet: $" ..useroldmoney[user_id].. " New Wallet: $" ..walletmoney[user_id].. " Value: $" ..difference.. " and was automatically banned. @everyone"
+								PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+								vRP.ban({source,"UserID: " ..user_id.. " was banned for modding money. Value: $" ..difference})
+							end})
+						end
+						if kickorban == "kick" then
+							vRP.getUserIdentity({user_id, function(identity)
+								local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedMoney.." in Cash in <5 seconds. Old Wallet: $" ..useroldmoney[user_id].. " New Wallet: $" ..walletmoney[user_id].. " Value: $" ..difference.. " and was automatically kicked. @everyone"
+								PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+								vRP.kick({source,"UserID: " ..user_id.. " was kicked for modding money. Value: $" ..difference})
+							end})
+						end
+					elseif useDiscord == false then
+						if kickorban == "ban" then
 							vRP.ban({source,"UserID: " ..user_id.. " was banned for modding money. Value: $" ..difference})
-						end})
-					end
-					if kickorban == "kick" then
-						vRP.getUserIdentity({user_id, function(identity)
-							local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedMoney.." in Cash in <5 seconds. Old Wallet: $" ..useroldmoney.. " New Wallet: $" ..usercurrentmoney.. " Value: $" ..difference.. " and was automatically kicked. @everyone"
-							PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+						elseif kickorban == "kick" then
 							vRP.kick({source,"UserID: " ..user_id.. " was kicked for modding money. Value: $" ..difference})
-						end})
-					end
-				elseif useDiscord == false then
-					if kickorban == "ban" then
-						vRP.ban({source,"UserID: " ..user_id.. " was banned for modding money. Value: $" ..difference})
-					elseif kickorban == "kick" then
-						vRP.kick({source,"UserID: " ..user_id.. " was kicked for modding money. Value: $" ..difference})
+						end
 					end
 				end
+			end	
+			useroldmoney[user_id] = walletmoney[user_id]
+		end
+	end
+	if BankCheck == true then
+		if useroldbmoney == nil then
+			useroldbmoney = bankmoney
+		elseif useroldbmoney ~= nil then
+			if bankmoney ~= nil and canGiveMoney == false then
+				local difference = bankmoney - useroldbmoney
+				if useroldbmoney ~= walletmoney then
+					if bankmoney > (useroldbmoney + moddedbMoney) then
+						print("UserID: " ..user_id.. " Has received more than "..moddedbMoney.." in <5 seconds. Old Bank: " ..useroldbmoney.. " New Bank: " ..bankmoney.. " Value: $" ..difference)
+						if useDiscord == true then
+							if kickorban == "ban" then
+								vRP.getUserIdentity({user_id, function(identity)
+									local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedbMoney.." in Bank Money in <5 seconds. Old Wallet: $" ..useroldbmoney.. " New Bank: $" ..bankmoney.. " Value: $" ..difference.. " and was automatically banned. @everyone"
+									PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+									vRP.ban({source,"UserID: " ..user_id.. " was banned for modding money. Value: $" ..difference})
+								end})
+							end
+							if kickorban == "kick" then
+								vRP.getUserIdentity({user_id, function(identity)
+									local newmessage = "UserID: " ..user_id.. " Has received more than "..moddedbMoney.." in Bank Money in <5 seconds. Old Wallet: $" ..useroldbmoney.. " New Bank: $" ..bankmoney.. " Value: $" ..difference.. " and was automatically kicked. @everyone"
+									PerformHttpRequest('INPUT LINK HERE', function(err, text, headers) end, 'POST', json.encode({username = identity.firstname.. " " ..identity.name , content = newmessage}), { ['Content-Type'] = 'application/json' })
+									vRP.kick({source,"UserID: " ..user_id.. " was kicked for modding money. Value: $" ..difference})
+								end})
+							end
+						elseif useDiscord == false then
+							if kickorban == "ban" then
+								vRP.ban({source,"UserID: " ..user_id.. " was banned for modding money. Value: $" ..difference})
+							elseif kickorban == "kick" then
+								vRP.kick({source,"UserID: " ..user_id.. " was kicked for modding money. Value: $" ..difference})
+							end
+						end
+					end
+				end	
+				useroldbmoney = bankmoney
 			end
-			useroldmoney = usercurrentmoney
 		end
 	end
 end
@@ -59,12 +104,9 @@ end
 RegisterNetEvent("honestycheck")
 AddEventHandler("honestycheck", function()
 	local source = source
-    vRPbs.GetWallet(source)
-end)
-
-AddEventHandler("vRP:playerSpawn", function(user_id, source, first_spawn)
-	if user_id ~= nil then
-		usercurrentmoney = vRP.getMoney({user_id})
+	if source then
+		local user_id = vRP.getUserId({source})
 		canGiveMoney = vRP.hasPermission({user_id,"player.givemoney"})
+		vRPbs.GetMoneyStatus(user_id)
 	end
 end)
